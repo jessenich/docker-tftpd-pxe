f2fdefb5e1ba44b9cfda8089eb4fa876262271e9
Jesse N <jesse@keplerdev.com>
2021-07-29 14:36:24 -0400

    added gitpod, updated editorconfig, updated github workflows

--- resources/run.sh
+++ resources/run.sh
@@ -4,5 +4,5 @@ set -eu;
 
 if [ -d /tftpboot/boot/root ]; then
     cp -af /tftpboot/boot/root/* /tftpboot;
-    exec in.tftpd -L -vvv -u ftp --secure --address 0.0.0.0:1069 $TFTPD_EXTRA_ARGS /tftpboot;
+    exec in.tftpd -L -vvv -u ftp --secure --address 0.0.0.0:1069 "${TFTPD_EXTRA_ARGS}" /tftpboot;
 fi
